46f9ffb476097f96816060623dd8dce3dab138ba
Thomas Teague <24764359+tteague19@users.noreply.github.com>
2024-12-01 09:10:31 -0500

    Implement a data loading function
    This change implements a function to load the data from a file.

--- src/cli/days/day_01.py
+++ src/cli/days/day_01.py
@@ -4,10 +4,101 @@ Implements a CLI that runs the exercise for Day 1 of the Advent of Code 2024.
 The problem description can be found at: https://adventofcode.com/2024/day/1
 """
 
+from pathlib import Path
+import polars as pl
 import click
 
 
+DATA_COL_NAMES = {"left", "right"}
+DATA_TYPE_ERROR_MESSAGE = " ".join(
+    ["The input data in column {col_name}", "is not of the expected type."]
+)
+COLUMN_NAME_ERROR_MESSAGE = " ".join(
+    [
+        "The input data frame does not have the expected column names.",
+        "Expected: {expected}",
+    ]
+)
+FILE_TYPE_ERROR_MESSAGE = " ".join(
+    [
+        "The input data file is not of the expected type.",
+        "Expected: .csv but received {file_type}.",
+    ]
+)
+
+
+def validate_input_data_file_type(input_file_path: Path) -> None:
+    """
+    Validate the file type of the input data for this day's exercise.
+
+    :param input_file_path: The path to the input file for this day's exercise.
+    :type input_file_path: Path
+    :raises AssertionError: If the input file is not of the expected type.
+    """
+    error_message = FILE_TYPE_ERROR_MESSAGE.format(
+        file_type=input_file_path.suffix
+    )
+    assert input_file_path.suffix == ".csv", error_message
+
+
+def validate_column_data_types(data_frame: pl.DataFrame) -> None:
+    """
+    Validate the data types in each column in the input data frame.
+
+    :param data_frame: The input data for this day's exercise.
+    :type data_frame: pl.DataFrame
+    :raises AssertionError: If at least one of the columns in the input
+        data frame does not contain data of the expected type.
+    """
+    for col_name in DATA_COL_NAMES:
+        error_message = DATA_TYPE_ERROR_MESSAGE.format(col_name=col_name)
+        assert data_frame[col_name].dtype == pl.Int64, error_message
+
+
+def validate_column_names(data_frame: pl.DataFrame) -> None:
+    """
+    Validate the column names in the input data frame.
+
+    :param data_frame: The input data for this day's exercise.
+    :type data_frame: pl.DataFrame
+    :raises AssertionError: If the input data frame does not have the expected
+        column names.
+    """
+    column_names = set(data_frame.columns)
+    error_message = COLUMN_NAME_ERROR_MESSAGE.format(expected=DATA_COL_NAMES)
+    assert column_names == DATA_COL_NAMES, error_message
+
+
+def load_input(file_path: Path) -> pl.DataFrame:
+    """
+    Load the input data for this day's exercise.
+
+    :param file_path: The path to the input file for this day's exercise.
+    :type file_path: Path
+    :return: The input data for this day's exercise in a data frame with two
+        columns: "left" and "right", each of which contain an integer in each
+        row.
+    :rtype: pl.DataFrame
+    """
+    validate_input_data_file_type(file_path)
+    data_frame = pl.read_csv(file_path)
+
+    validate_column_names(data_frame)
+    validate_column_data_types(data_frame)
+
+    return data_frame
+
+
 @click.command()
-def day_01():
-    """Run the solution for Day 1 of the Advent of Code 2024."""
+@click.argument(
+    "input_file_path",
+    type=click.Path(exists=True, readable=True, path_type=Path),
+)
+def day_01(input_file_path: Path) -> None:
+    """
+    Compute the solution for Day 1 of the Advent of Code 2024.
+
+    :param input_file_path: The path to the input file for this day's exercise.
+    :type input_file_path: Path
+    """
     click.echo("Day 1: Not yet implemented")
--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -1,13 +1,51 @@
 """Implements tests for the code for Day 1 of the Advent of Code."""
 
+from pathlib import Path
+
 from click.testing import CliRunner
+import pytest
 
-from src.cli.days.day_01 import day_01
+from src.cli.days.day_01 import DATA_COL_NAMES, day_01, load_input
 
 
-def test_day_01():
-    """Test for Day 1."""
+DATA_DIR = Path(__file__).parents[1] / "data" / "day_01"
+DATA_FILE_PATH = DATA_DIR / "distances.csv"
+INVALID_DATA_COL_NAMES_FILE_PATH = DATA_DIR / "invalid-col-names.csv"
+INVALID_DATA_FILE_TYPE_FILE_PATH = DATA_DIR / "invalid-file-type.txt"
+INVALID_DATA_TYPES_FILE_PATH = DATA_DIR / "invalid-data-types.csv"
+
+
+def test_day_01_with_valid_input():
+    """Test for Day 1 with valid input."""
     runner = CliRunner()
-    result = runner.invoke(day_01)
+    result = runner.invoke(day_01, [DATA_FILE_PATH.as_posix()])
     assert result.exit_code == 0
     assert "Day 1: Not yet implemented" in result.output
+
+
+def test_load_input_with_invalid_column_names():
+    """Test for Day 1 with invalid column names in the input data."""
+
+    with pytest.raises(AssertionError):
+        load_input(file_path=INVALID_DATA_COL_NAMES_FILE_PATH)
+
+
+def test_load_input_with_invalid_data_file_type():
+    """Test for Day 1 with an invalid file type for the input data."""
+
+    with pytest.raises(AssertionError):
+        load_input(file_path=INVALID_DATA_FILE_TYPE_FILE_PATH)
+
+
+def test_load_input_with_invalid_data_types():
+    """Test for Day 1 with invalid data types in the input data."""
+
+    with pytest.raises(AssertionError):
+        load_input(file_path=INVALID_DATA_TYPES_FILE_PATH)
+
+
+def test_load_input_with_valid_data():
+    """Test for Day 1 with valid data."""
+    data_frame = load_input(file_path=DATA_FILE_PATH)
+
+    assert data_frame.shape[1] == len(DATA_COL_NAMES)
